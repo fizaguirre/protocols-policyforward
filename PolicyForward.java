19e9872a9f744db353fcdf701e5b5683b96c6712
fmizaguirre@inf.ufrgs.br <fmizaguirre@inf.ufrgs.br>
2017-06-28 03:09:27 -0300

    Getting path and adding flow

--- PolicyForward.java
+++ PolicyForward.java
@@ -1,73 +1,52 @@
 package net.floodlightcontroller.policyforward;
 
-import java.lang.invoke.SwitchPoint;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-import org.projectfloodlight.openflow.protocol.OFFlowAdd;
-import org.projectfloodlight.openflow.protocol.OFFlowMod;
 import org.projectfloodlight.openflow.protocol.OFFlowModCommand;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFPacketOut;
-import org.projectfloodlight.openflow.protocol.OFPortDesc;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.protocol.action.OFAction;
-import org.projectfloodlight.openflow.protocol.action.OFActions;
 import org.projectfloodlight.openflow.protocol.match.Match;
 import org.projectfloodlight.openflow.protocol.match.MatchField;
-import org.projectfloodlight.openflow.types.DatapathId;
 import org.projectfloodlight.openflow.types.EthType;
-import org.projectfloodlight.openflow.types.MacAddress;
-import org.projectfloodlight.openflow.types.OFBufferId;
+import org.projectfloodlight.openflow.types.ICMPv4Type;
 import org.projectfloodlight.openflow.types.OFPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
-import net.floodlightcontroller.core.IListener.Command;
 import net.floodlightcontroller.core.internal.IOFSwitchService;
-import net.floodlightcontroller.core.internal.ISwitchDriverRegistry;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
-import net.floodlightcontroller.core.IOFSwitchListener;
-import net.floodlightcontroller.core.PortChangeType;
 import net.floodlightcontroller.core.module.*;
-import net.floodlightcontroller.core.types.NodePortTuple;
 import net.floodlightcontroller.devicemanager.IDevice;
 import net.floodlightcontroller.devicemanager.IDeviceService;
 import net.floodlightcontroller.devicemanager.SwitchPort;
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryListener;
 import net.floodlightcontroller.linkdiscovery.ILinkDiscoveryService;
-import net.floodlightcontroller.linkdiscovery.Link;
-import net.floodlightcontroller.linkdiscovery.internal.LinkInfo;
 import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.Ethernet;
-import net.floodlightcontroller.packet.IPacket;
+import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.routing.*;
 import net.floodlightcontroller.topology.ITopologyService;
-import net.floodlightcontroller.util.OFMessageUtils;
 
 public class PolicyForward extends ForwardingBase implements IOFMessageListener, IFloodlightModule, ILinkDiscoveryListener {
 	
 	protected static Logger logger;
 	protected BlockingQueue<LDUpdate> lduUpdate; //Queue to hold pending topology updates
 	protected static ILinkDiscoveryService linkDiscoveryService; //LLDP service. It handles the LLDP protocol. We need to subscribe to it to listen for LLDP topology events.
-	protected IFloodlightProviderService floodlightProvider;
 	protected ITopologyService topologyService;
 	protected IRoutingService routingManager;
-	protected Topology topo;
-	protected IOFSwitchService switchService;
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
@@ -103,22 +82,21 @@ public class PolicyForward extends ForwardingBase implements IOFMessageListener,
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
 		// TODO Auto-generated method stub
 		super.init();
+		this.floodlightProviderService = context.getServiceImpl(IFloodlightProviderService.class);
+		this.switchService = context.getServiceImpl(IOFSwitchService.class);
 		logger = LoggerFactory.getLogger(PolicyForward.class);
 		linkDiscoveryService = context.getServiceImpl(ILinkDiscoveryService.class);
-		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
 		topologyService = context.getServiceImpl(ITopologyService.class);
 		routingManager = context.getServiceImpl(IRoutingService.class);
-		switchService = context.getServiceImpl(IOFSwitchService.class);
 	}
 
 	@Override
 	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
 		// TODO Auto-generated method stub
-		//super.startUp();
+		super.startUp();
 		logger.info("Starting up PolicyForward module");
 		linkDiscoveryService.addListener(this);
 		lduUpdate = new LinkedBlockingQueue<LDUpdate>();
-		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
 		routingManager.setMaxPathsToCompute(10);
 	}
 
@@ -131,12 +109,13 @@ public class PolicyForward extends ForwardingBase implements IOFMessageListener,
 	
 	@Override
 	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-
+		logger.info("Packet in type: {}", msg.getType());
 		switch(msg.getType())
 		{
 		case PACKET_IN:
 			Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 			logger.info("Packet from src mac addr {} received from switch {}", eth.getSourceMACAddress(), sw.getId().toString());
+			logger.info("Packet eth payload: {}", eth.getPayload());
 			
 			if ( eth.getEtherType() == EthType.ARP) {
 				// SEND PACKET TO ALL OTHER PORTS OF THE SWITCH
@@ -145,11 +124,11 @@ public class PolicyForward extends ForwardingBase implements IOFMessageListener,
 				if ( arp.getOpCode() == ARP.OP_REQUEST)
 					this.doBroacastPacket(sw, msg);
 				else if ( arp.getOpCode() == ARP.OP_REPLY) {
-					//TODO
-					//Get path between nodes
-					logger.info("Get path between nodes");
-					this.doFlow(sw, msg, cntx);					
+					this.doFlow(sw, msg, cntx, eth);			
 				}
+			}
+			else if (eth.getEtherType() == EthType.IPv4 ) {
+				this.doFlow(sw, msg, cntx, eth);
 			}
 			
 			break;
@@ -160,7 +139,7 @@ public class PolicyForward extends ForwardingBase implements IOFMessageListener,
 		return Command.CONTINUE;
 	}
 	
-	private void doFlow(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+	private void doFlow(IOFSwitch sw, OFMessage msg, FloodlightContext cntx, Ethernet eth) {
 		IDevice dstHost = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_DST_DEVICE);
 		IDevice srcHost = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_SRC_DEVICE);
 		
@@ -181,13 +160,37 @@ public class PolicyForward extends ForwardingBase implements IOFMessageListener,
 		Path path = routingManager.getPath(sw.getId(), pi.getInPort(), destPortSwitch.getNodeId(), destPortSwitch.getPortId());
 		logger.info("Path found {}", path);
 		
-		Match matchRule = sw.getOFFactory().buildMatch()
+		Match matchRule = buildMatch(sw, msg, cntx, eth);
+		
+		pushRoute(path, matchRule, pi, sw.getId(), DEFAULT_FORWARDING_COOKIE, cntx, false, OFFlowModCommand.ADD);
+	}
+	
+	private Match buildMatch(IOFSwitch sw, OFMessage msg, FloodlightContext cntx, Ethernet eth) {
+		IDevice dstHost = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_DST_DEVICE);
+		IDevice srcHost = IDeviceService.fcStore.get(cntx, IDeviceService.CONTEXT_SRC_DEVICE);
+		
+		Match matchRule = null;
+		
+		if (eth.getEtherType() == EthType.ARP) {
+			logger.info("Building ARP match.");
+			matchRule = sw.getOFFactory().buildMatch()
 				.setExact(MatchField.ETH_TYPE, EthType.ARP)
 				.setExact(MatchField.ETH_SRC, srcHost.getMACAddress())
 				.setExact(MatchField.ETH_DST, dstHost.getMACAddress())
 				.build();
+		}else if (eth.getEtherType() == EthType.IPv4) {
+			logger.info("Building IVv4 match.");
+			IPv4 ipv4 = (IPv4) eth.getPayload();
+			
+			
+			matchRule = sw.getOFFactory().buildMatch()
+				.setExact(MatchField.IPV4_SRC, ipv4.getSourceAddress())
+				.setExact(MatchField.IPV4_DST, ipv4.getDestinationAddress())
+				.build();
+			
+		}
 		
-		pushRoute(path, matchRule, pi, sw.getId(), DEFAULT_FORWARDING_COOKIE, cntx, false, OFFlowModCommand.ADD);
+		return matchRule;
 	}
 	
 	private void doBroacastPacket(IOFSwitch sw, OFMessage m) {
@@ -222,15 +225,7 @@ public class PolicyForward extends ForwardingBase implements IOFMessageListener,
 	public void linkDiscoveryUpdate(List<LDUpdate> updateList) {
 		
 		lduUpdate.addAll(updateList); //Get a list of link layer discovery updates
-		//this.buildTopology();
 	}
 	
-	protected void buildTopology()
-	{
-		Map<NodePortTuple, Set<Link>> npt = linkDiscoveryService.getPortLinks();
-		Map<Link, LinkInfo> links = linkDiscoveryService.getLinks();
-		//topo = new Topology(npt, links);
-		//topo.showTopology();
-	}
 
 }
